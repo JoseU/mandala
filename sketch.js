af4ddf7674ec6a98f3d508a1f3497c51c7cf048a
R3N0 <renomartisan@gmail.com>
2015-10-19 16:09:51 -0500

    Setup responsive del escenario
    Establecimiento de las reglas de tamaño y remuestreo del tamaño del
    canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+var alto;
+var origen;
+var canvas;
+var margen = 10;
+
 function setup() {
-  
+  alto = windowHeight - margen;
+  origen = createVector(windowWidth/2 - alto/2, windowHeight/2 - alto/2);
+  canvas = createCanvas(alto, alto);
+  canvas.position(origen.x,origen.y);
+  background(255);
 }
 
 function draw() {
   
+}
+
+function windowResized() {
+  alto = windowHeight - margen;
+  origen.x = windowWidth/2 - alto/2;
+  origen.y = windowHeight/2 - alto/2;
+  canvas.position(origen.x,origen.y);
+  resizeCanvas(alto, alto);
+  background(255);
 }
