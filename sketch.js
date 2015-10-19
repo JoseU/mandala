b6fc9c67bfa10764822f1e407b2efbc400ffb7fe
R3N0 <renomartisan@gmail.com>
2015-10-19 16:16:09 -0500

    Perfeccionamiento del resize
    Implementación de cambio de tamaño al decrecer los bordes laterales

--- sketch.js
+++ sketch.js
@@ -1,25 +1,34 @@
-var alto;
+var radio;
 var origen;
 var canvas;
 var margen = 10;
 
 function setup() {
-  alto = windowHeight - margen;
-  origen = createVector(windowWidth/2 - alto/2, windowHeight/2 - alto/2);
-  canvas = createCanvas(alto, alto);
-  canvas.position(origen.x,origen.y);
+  testSize();
+  origen = createVector(windowWidth / 2 - radio / 2, windowHeight / 2 - radio / 2);
+  canvas = createCanvas(radio, radio);
+  canvas.position(origen.x, origen.y);
   background(255);
 }
 
 function draw() {
-  
+
 }
 
 function windowResized() {
-  alto = windowHeight - margen;
-  origen.x = windowWidth/2 - alto/2;
-  origen.y = windowHeight/2 - alto/2;
-  canvas.position(origen.x,origen.y);
-  resizeCanvas(alto, alto);
+  testSize();
+  origen.x = windowWidth / 2 - radio / 2;
+  origen.y = windowHeight / 2 - radio / 2;
+  canvas.position(origen.x, origen.y);
+  resizeCanvas(radio, radio);
   background(255);
+}
+
+
+function testSize(){
+  if (windowWidth >= windowHeight) {
+    radio = windowHeight - margen;
+  } else {
+    radio = windowWidth - margen;
+  }
 }
